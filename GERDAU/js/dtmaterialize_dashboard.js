fe76bdb3df046fe2a36c944ebba710bc4d798da3
Luiz Alberto de Andrade Coelho <luiz.coelho@mc1tecnologia.onmicrosoft.com>
2018-04-16 17:09:10 -0300

    initial

---
+++ GERDAU/js/dtmaterialize_dashboard.js
@@ -0,0 +1,123 @@
+﻿(function (window, document, undefined) {
+
+    var factory = function ($, DataTable) {
+        "use strict";
+
+        $.extend(true, DataTable.defaults, {
+            "dom": "<'hiddensearch row'f'>" +
+                "tr" +
+                "<'table-footer'lipB'>",
+            "renderer": 'material',
+            "select": {
+                style: 'single'
+            },            
+            "scrollCollapse": true
+        });
+
+        /* Default class modification */
+        $.extend(DataTable.ext.classes, {
+            sWrapper: "dataTables_wrapper",
+            sFilterInput: "form-control input-sm",
+            sLengthSelect: "form-control input-sm"
+        });
+
+        /* Bootstrap paging button renderer */
+        DataTable.ext.renderer.pageButton.material = function (settings, host, idx, buttons, page, pages) {
+            var api = new DataTable.Api(settings);
+            var classes = settings.oClasses;
+            var lang = settings.oLanguage.oPaginate;
+            var btnDisplay, btnClass, counter = 0;
+
+            var attach = function (container, buttons) {
+                var i, ien, node, button;
+                var clickHandler = function (e) {
+                    e.preventDefault();
+                    if (!$(e.currentTarget).hasClass('disabled')) {
+                        api.page(e.data.action).draw(false);
+                    }
+                };
+
+                for (i = 0, ien = buttons.length; i < ien; i++) {
+                    button = buttons[i];
+
+                    if ($.isArray(button)) {
+                        attach(container, button);
+                    } else {
+                        btnDisplay = '';
+                        btnClass = '';
+
+                        switch (button) {
+
+                            case 'first':
+                                btnDisplay = lang.sFirst;
+                                btnClass = button + (page > 0 ? '' : ' disabled');
+                                break;
+
+                            case 'previous':
+                                btnDisplay = '<i class="material-icons">chevron_left</i>';
+                                btnClass = button + (page > 0 ? '' : ' disabled');
+                                break;
+
+                            case 'next':
+                                btnDisplay = '<i class="material-icons">chevron_right</i>';
+                                btnClass = button + (page < pages - 1 ? '' : ' disabled');
+                                break;
+
+                            case 'last':
+                                btnDisplay = lang.sLast;
+                                btnClass = button + (page < pages - 1 ? '' : ' disabled');
+                                break;
+
+                        }
+
+                        if (btnDisplay) {
+                            node = $('<li>', {
+                                'class': classes.sPageButton + ' ' + btnClass,
+                                'id': idx === 0 && typeof button === 'string' ? settings.sTableId + '_' + button : null
+                            })
+                            .append($('<a>', {
+                                'href': '#',
+                                'aria-controls': settings.sTableId,
+                                'data-dt-idx': counter,
+                                'tabindex': settings.iTabIndex
+                            })
+                              .html(btnDisplay)
+                            )
+                            .appendTo(container);
+
+                            settings.oApi._fnBindAction(node, {
+                                action: button
+                            }, clickHandler
+                            );
+
+                            counter++;
+                        }
+                    }
+                }
+            };
+
+            // IE9 throws an 'unknown error' if document.activeElement is used
+            // inside an iframe or frame. 
+            var activeEl;
+
+            try {
+                // Because this approach is destroying and recreating the paging
+                // elements, focus is lost on the select button which is bad for
+                // accessibility. So we want to restore focus once the draw has
+                // completed
+                activeEl = $(document.activeElement).data('dt-idx');
+            } catch (e) { }
+
+            attach($(host).empty().html('<ul class="material-pagination"/>').children('ul'), buttons);
+
+            if (activeEl) {
+                $(host).find('[data-dt-idx=' + activeEl + ']').focus();
+            }
+        };
+
+    };
+
+    factory(jQuery, jQuery.fn.dataTable);
+
+})(window, document);
+
